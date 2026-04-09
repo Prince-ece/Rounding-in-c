9f46e049222d66adcf288b3857437c43df7b3024
Prince <pt8002739613@gmail.com>
2026-04-09 15:48:45 +0530

    Enhance prince.c with comments for better understanding
    Added comments for clarity in rounding logic.

--- prince.c
+++ prince.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 int main() {
     float a;
+    // Taking input from User
     printf("Enter the number to roundoff: ");
     scanf("%f", &a);
-    int b = (int)a;
+    
+    int b = (int)a; //seperating integeral part from input
+    // rounding logic
     if (a-b >= 0.5){
       printf("%d", b+1);
     }
